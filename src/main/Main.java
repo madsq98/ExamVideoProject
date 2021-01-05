35528bba7f299145e7f556d0865eefb44f781ed1
Emil Hallum Hansen <emil875b@easv365.dk>
2021-01-05 10:41:11 +0100

    dimentions

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gui/mainView.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
     }
 
