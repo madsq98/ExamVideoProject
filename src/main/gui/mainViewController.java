3ea90a9871236451273bb99acd3721404432e3a4
ceci6182 <ceci6182@easv365.dk>
2021-01-05 10:36:35 +0100

    Update mainViewController.java

--- src/main/gui/mainViewController.java
+++ src/main/gui/mainViewController.java
@@ -1,4 +1,17 @@
 package main.gui;
 
+import javafx.event.ActionEvent;
+
 public class mainViewController {
+    public void handleAddCategory(ActionEvent actionEvent) {
+    }
+
+    public void handleRemoveCategory(ActionEvent actionEvent) {
+    }
+
+    public void handleAddMovie(ActionEvent actionEvent) {
+    }
+
+    public void handleRemoveMovie(ActionEvent actionEvent) {
+    }
 }
