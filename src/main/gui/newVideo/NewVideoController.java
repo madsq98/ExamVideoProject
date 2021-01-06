cd30902ab9cb85d3bc66579619da035c197fbfdd
ceci6182 <ceci6182@easv365.com>
2021-01-06 09:44:47 +0100

    NewVideo

--- src/main/gui/newVideo/NewVideoController.java
+++ src/main/gui/newVideo/NewVideoController.java
@@ -1,4 +1,20 @@
 package main.gui.newVideo;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class NewVideoController {
+    public TextField txtVideoTitle;
+    public TextField txtVideoGenre;
+    public TextField txtVideoRating;
+    public TextField txtVideoFile;
+
+    public void handleChooseVideoFile(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleSave(ActionEvent actionEvent) {
+    }
 }
