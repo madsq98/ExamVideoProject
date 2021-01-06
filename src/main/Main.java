795ba0a46c1bbb1781fd3714a50b3864f74f7df9
Emil Hallum Hansen <emil875b@easv365.dk>
2021-01-06 11:31:18 +0100

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gui/mainView.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Private Movie Collection");
         primaryStage.setScene(new Scene(root, 1024, 768));
         primaryStage.show();
     }
