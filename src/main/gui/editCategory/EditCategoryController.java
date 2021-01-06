1e58ee9a30fb800f0a94e220e78ff65b04a602b5
Emil Hallum Hansen <emil875b@easv365.dk>
2021-01-06 11:37:58 +0100

    close on

--- src/main/gui/editCategory/EditCategoryController.java
+++ src/main/gui/editCategory/EditCategoryController.java
@@ -1,20 +1,27 @@
 package main.gui.editCategory;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import main.bll.CategoryManager;
 
 public class EditCategoryController {
     public TextField txtCategory;
+    public Button btnCancelEditCategory;
     private CategoryManager cMan;
 
-    public void handleCancel(ActionEvent actionEvent) {
-    }
+    public void handleCancel(ActionEvent actionEvent) {closeWin();    }
 
     public void handleSave(ActionEvent actionEvent) {
     }
 
     public void setManager(CategoryManager cMan) {
         this.cMan = cMan;
+    }
+
+    private void closeWin(){
+        Stage stage = (Stage) btnCancelEditCategory.getScene().getWindow();
+        stage.close();
     }
 }
