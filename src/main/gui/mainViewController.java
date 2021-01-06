72f7e9dfbfeb66d79f7d431765a0fe7173e71252
Emil Hallum Hansen <emil875b@easv365.dk>
2021-01-06 10:20:01 +0100

    ???

--- src/main/gui/mainViewController.java
+++ src/main/gui/mainViewController.java
@@ -4,7 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import main.be.Category;
+import main.be.Video;
 import main.bll.CategoryManager;
 import main.bll.VideoManager;
 import main.gui.newCategory.NewCategoryController;
@@ -14,24 +17,39 @@ import java.io.IOException;
 
 public class mainViewController {
 /*
-        Setting managers
+        GUI objects
  */
+    public ListView lstviewCategories;
+
+/*
+            Setting managers
+*/
     private CategoryManager cMan = new CategoryManager();
     private VideoManager vMan = new VideoManager();
 
+/*
+        Controls regarding functionality
+ */
+    private Category selectedCategory;
+    private Video selectedVideo;
+
+/*
+        Category buttons
+ */
     public void handleAddCategory(ActionEvent actionEvent) {openNewCategory("newCategory/NewCategoryView.fxml");}
 
     public void handleRemoveCategory(ActionEvent actionEvent) {
+        
     }
 
-    public void handleAddMovie(ActionEvent actionEvent) {
-        openNewMovie("newVideo/NewVideoView.fxml");
-    }
+/*
+        Movie buttons
+ */
+    public void handleAddMovie(ActionEvent actionEvent) {openNewMovie("newVideo/NewVideoView.fxml");}
 
     public void handleRemoveMovie(ActionEvent actionEvent) {
 
     }
-
 
 /*
         Methods to open Views
