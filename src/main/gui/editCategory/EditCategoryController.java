32b320d2d33244b9277f4a51e1c558ae038c20ec
ceci6182 <ceci6182@easv365.com>
2021-01-06 10:19:38 +0100

    gui

--- src/main/gui/editCategory/EditCategoryController.java
+++ src/main/gui/editCategory/EditCategoryController.java
@@ -1,4 +1,14 @@
 package main.gui.editCategory;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class EditCategoryController {
+    public TextField txtCategory;
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleSave(ActionEvent actionEvent) {
+    }
 }
