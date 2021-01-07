f008202f0e9b2f6c23aa770d0dbe1e8129b8b959
ceci6182 <ceci6182@easv365.dk>
2021-01-07 10:47:06 +0100

    video views controller views

--- src/main/gui/newVideo/NewVideoController.java
+++ src/main/gui/newVideo/NewVideoController.java
@@ -3,20 +3,19 @@ package main.gui.newVideo;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import main.bll.VideoManager;
+import java.io.File;
+
 
 public class NewVideoController {
     public TextField txtVideoTitle;
-    public TextField txtVideoGenre;
-    public TextField txtVideoRating;
     public TextField txtVideoFile;
     public Button btnCancelNewVideo;
 
-    private VideoManager vMan;
 
-    public void handleChooseVideoFile(ActionEvent actionEvent) {
-    }
+    private VideoManager vMan;
 
     public void handleCancel(ActionEvent actionEvent) {
         closeWin();
@@ -26,6 +25,19 @@ public class NewVideoController {
     }
 
     public void setManager(VideoManager vMan) {this.vMan = vMan;}
+
+    public void handleChooseVideoFile(ActionEvent actionEvent) {
+        try {
+            Stage stage = (Stage) btnCancelNewVideo.getScene().getWindow();
+            FileChooser fc = new FileChooser();
+            fc.setTitle("Choose Video...");
+            File file = fc.showOpenDialog(stage);
+            String filePath = file.toString().replaceAll("\\\\","/");
+            txtVideoFile.setText(filePath);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void closeWin(){
         Stage stage = (Stage) btnCancelNewVideo.getScene().getWindow();
