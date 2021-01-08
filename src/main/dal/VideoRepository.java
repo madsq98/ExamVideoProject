1e17359fd7c147ea92e6e786509c2c05754f7451
madsq98 <mads.q@outlook.com>
2021-01-08 09:39:38 +0100

    Added some SQL

--- src/main/dal/VideoRepository.java
+++ src/main/dal/VideoRepository.java
@@ -2,18 +2,17 @@ package main.dal;
 
 import javafx.collections.ObservableList;
 import main.be.Video;
+import main.util.DateConverter;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class VideoRepository {
-
+    private SqlConnectionHandler sqlClass;
     private Connection connection;
 
-    public VideoRepository() {
-
-        //Create JDBC connection to MSSQL database here
-
-
+    public VideoRepository() throws SQLException {
+        sqlClass = new SqlConnectionHandler();
+        connection = sqlClass.getConnection();
     }
 
     public ObservableList<Video> loadVideos() {
@@ -21,14 +20,30 @@ public class VideoRepository {
         return null;
     }
 
-    public void delete(Video videoToDelete){
-
-        String query = "DELETE FROM MOVIE WHERE ID = ?;";
+    public void delete(Video videoToDelete) throws SQLException {
+        String query = "DELETE FROM MOVIE WHERE ID = "+videoToDelete.getId()+";";
+        PreparedStatement st = connection.prepareStatement(query);
+        st.executeUpdate();
     }
 
-    public void add(Video videoToAdd){
-        //INSERT INTO movie - adds a video object to the table movie
-        //Remember to fiddle with getGeneratedKeys() to get the Insert ID
+    public int add(Video videoToAdd) throws SQLException {
+        String query = "INSERT INTO MOVIE (name, rating, path, lastview) VALUES (?, ?, ?, ?);";
+        PreparedStatement st = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+        st.setString(1, videoToAdd.getName());
+        st.setDouble(2, videoToAdd.getRating());
+        st.setString(3, videoToAdd.getPath());
+        st.setDate(4, java.sql.Date.valueOf(videoToAdd.getLastView()));
+
+        int affectedRows = st.executeUpdate();
+
+        ResultSet keys = st.getGeneratedKeys();
+
+        if(keys.next()) {
+            return keys.getInt(1);
+        } else {
+            return 0;
+        }
     }
 
     public void update(Video videoToUpdate){
