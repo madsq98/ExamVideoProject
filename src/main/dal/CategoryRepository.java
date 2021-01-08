5ffaac87bd862d5db96cef57896aee3ed7eaf614
madsq98 <mads.q@outlook.com>
2021-01-08 10:12:20 +0100

    Update CategoryRepository.java

--- src/main/dal/CategoryRepository.java
+++ src/main/dal/CategoryRepository.java
@@ -12,7 +12,6 @@ public class CategoryRepository {
     private SqlConnectionHandler sqlClass;
     private Connection connection;
     private PreparedStatement preparedStatement = null;
-    private Connection connect = null;
     private Statement statement = null;
     private ResultSet resultSet = null;
 
@@ -22,67 +21,46 @@ public class CategoryRepository {
     }
 
 
-    public ObservableList<Category> loadCategories(){
-        try{
-            ObservableList<Category> categories = FXCollections.observableArrayList();
-            String query = "SELECT * FROM Category ORDER BY id";
-            statement = connect.createStatement();
-            resultSet = statement.executeQuery(query);
-            while(resultSet.next()) {
-                Category  c = new Category(resultSet.getString("name"));
-                categories.add(c);
-            }
-            return null;
-        }catch (Exception e){
-            e.printStackTrace();
-            return null;
+    public ObservableList<Category> loadCategories() throws SQLException {
+        ObservableList<Category> categories = FXCollections.observableArrayList();
+        String query = "SELECT * FROM Category ORDER BY id";
+        statement = connection.createStatement();
+        resultSet = statement.executeQuery(query);
+        while(resultSet.next()) {
+            Category  c = new Category(resultSet.getString("name"));
+            categories.add(c);
         }
+        return categories;
     }
 
-    public void delete(Category categoryToDelete) {
-        try {
-            String sql = "DELETE FROM Category WHERE ID = " + categoryToDelete.getId() + ";";
-            PreparedStatement st = connection.prepareStatement(sql);
-            st.executeUpdate();
-        } catch(SQLException e) {
-            e.printStackTrace();
-        }
+    public void delete(Category categoryToDelete) throws SQLException {
+        String sql = "DELETE FROM Category WHERE ID = " + categoryToDelete.getId() + ";";
+        PreparedStatement st = connection.prepareStatement(sql);
+        st.executeUpdate();
     }
 
-    public int add(Category categoryToAdd){
+    public int add(Category categoryToAdd) throws SQLException {
         int returnId = -1;
-        try{
-            String name = categoryToAdd.getName();
-            String query ="INSERT INTO Category SET name ='"+name+"'";
-            preparedStatement = connect.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-            preparedStatement.executeUpdate();
-            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
-            if(generatedKeys.next()){
-                returnId = generatedKeys.getInt(1);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
+        String name = categoryToAdd.getName();
+        String query ="INSERT INTO Category SET name ='"+name+"'";
+        preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.executeUpdate();
+        ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+        if(generatedKeys.next()){
+            returnId = generatedKeys.getInt(1);
         }
         return returnId;
     }
 
-    public void update(Category categoryToUpdate){
-        try {
-            String name = categoryToUpdate.getName();
-            int id = categoryToUpdate.getId();
-            String query = "UPDATE Category SET name = '"+name+"' WHERE id = '"+id+"'";
-            preparedStatement = connect.prepareStatement(query);
-            preparedStatement.executeUpdate();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+    public void update(Category categoryToUpdate) throws SQLException {
+        String name = categoryToUpdate.getName();
+        int id = categoryToUpdate.getId();
+        String query = "UPDATE Category SET name = '"+name+"' WHERE id = '"+id+"'";
+        preparedStatement = connection.prepareStatement(query);
+        preparedStatement.executeUpdate();
     }
 
     public void saveAllLinks(Category c){
-
-    }
-
-    public void ObservableList(Category c){
 
     }
 
