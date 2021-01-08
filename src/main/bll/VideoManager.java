29d57e789454e7932582aab913c6b16ecf738e6f
madsq98 <mads.q@outlook.com>
2021-01-08 09:55:33 +0100

    SQL Fix

--- src/main/bll/VideoManager.java
+++ src/main/bll/VideoManager.java
@@ -5,24 +5,32 @@ import javafx.collections.ObservableList;
 import main.be.Video;
 import main.dal.VideoRepository;
 
+import java.sql.SQLException;
+
 public class VideoManager {
     private ObservableList<Video> videos;
     private VideoRepository vRepo;
 
-    public VideoManager() {
+    public VideoManager() throws SQLException {
         videos = FXCollections.observableArrayList();
         vRepo = new VideoRepository();
+
+        videos.addAll(vRepo.loadVideos());
     }
 
-    public void add(Video v) {
+    public void add(Video v) throws SQLException {
+        int newId = vRepo.add(v);
+        v.setId(newId);
         videos.add(v);
     }
 
-    public void delete(Video v) {
+    public void delete(Video v) throws SQLException {
+        vRepo.delete(v);
         videos.remove(v);
     }
 
-    public void replace(Video s, Video r) {
+    public void replace(Video s, Video r) throws SQLException {
+        vRepo.update(r);
         for(int i = 0; i < videos.size(); i++) {
             if(videos.get(i) == s) {
                 videos.set(i,r);
