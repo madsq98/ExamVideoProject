c29b103189f5d48d6c3614cc8d929fce9ca84f28
madsq98 <mads.q@outlook.com>
2021-01-08 09:47:47 +0100

    Update VideoRepository.java

--- src/main/dal/VideoRepository.java
+++ src/main/dal/VideoRepository.java
@@ -1,10 +1,12 @@
 package main.dal;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import main.be.Video;
 import main.util.DateConverter;
 
 import java.sql.*;
+import java.time.LocalDate;
 
 public class VideoRepository {
     private SqlConnectionHandler sqlClass;
@@ -15,14 +17,34 @@ public class VideoRepository {
         connection = sqlClass.getConnection();
     }
 
-    public ObservableList<Video> loadVideos() {
-        //SELECT * FROM movie - returns a list of all videos from table movie
-        return null;
+    public ObservableList<Video> loadVideos() throws SQLException {
+        String query = "SELECT * FROM MOVIE";
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        
+        ObservableList<Video> returnList = FXCollections.observableArrayList();
+
+        while(rs.next()) {
+            int id = rs.getInt("ID");
+            String name = rs.getString("name");
+            double rating = rs.getDouble("rating");
+            String path = rs.getString("path");
+            LocalDate lastView = rs.getDate("lastview").toLocalDate();
+
+            Video v = new Video(name,path);
+            v.setId(id);
+            v.setRating(rating);
+            v.setLastView(lastView);
+            returnList.add(v);
+        }
+
+        return returnList;
     }
 
     public void delete(Video videoToDelete) throws SQLException {
-        String query = "DELETE FROM MOVIE WHERE ID = "+videoToDelete.getId()+";";
+        String query = "DELETE FROM MOVIE WHERE ID = ?;";
         PreparedStatement st = connection.prepareStatement(query);
+        st.setInt(1, videoToDelete.getId());
         st.executeUpdate();
     }
 
@@ -46,7 +68,15 @@ public class VideoRepository {
         }
     }
 
-    public void update(Video videoToUpdate){
-        //UPDATE movie SET name = ?..... WHERE id = '+ videoToUpdate.getId() +'
+    public void update(Video videoToUpdate) throws SQLException {
+        String query = "UPDATE MOVIE SET name = ?, rating = ?, path = ?, lastview = ? WHERE ID = ?;";
+        PreparedStatement st = connection.prepareStatement(query);
+
+        st.setString(1, videoToUpdate.getName());
+        st.setDouble(2, videoToUpdate.getRating());
+        st.setString(3, videoToUpdate.getPath());
+        st.setDate(4, java.sql.Date.valueOf(videoToUpdate.getLastView()));
+
+        st.executeUpdate();
     }
 }
