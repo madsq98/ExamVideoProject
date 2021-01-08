a6361954d74f54b2bfa8fbdc4a25deeeb26b7dc2
ceci6182 <ceci6182@easv365.dk>
2021-01-08 11:39:04 +0100

    Update mainViewController.java

--- src/main/gui/mainViewController.java
+++ src/main/gui/mainViewController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import main.be.Category;
 import main.be.Video;
